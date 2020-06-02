17cd74e67e6654d5f1ea6d5694fc27cce5682c8a
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-02 12:48:49 +0530

    Next Largest Number

--- NextLargerElement/Solution.java
+++ NextLargerElement/Solution.java
@@ -1,30 +1,35 @@
 class Solution{
 static int[] arrayAnswer;
 public static void main(String[] args) {
-    int[] array={1,3,4,2};
+    int[] array={4,3,2,1};
     arrayAnswer=new int[array.length];
-
+    for(int i =0;i<arrayAnswer.length;i++){
+      arrayAnswer[i]=-1;
+    }
     find(array,0,0);
   }
 
 
 static void find(int[] array,int start,int end){
   // base cases
-  if(end==array.length-1 && start!=array.length-1){
+  if(end==array.length && start!=array.length){
+
     find(array,start+1,start+1);
   }
   //base cases
-  else if(start==array.length-1 && end==array.length-1){
+  else if(start==array.length && end==array.length){
     for(int i : arrayAnswer){
       System.out.println(i);
     }
   }
   else{
        if(array[end]>array[start]){
+
          arrayAnswer[start]=array[end];
          find(array,start+1,start+1);
        }
        else{
+
            find(array,start,end+1);
        }
 
