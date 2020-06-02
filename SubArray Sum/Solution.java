a07967294a82dc78f21761367b97aa8e23d202df
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-03 00:40:00 +0530

    Algorithms

---
+++ SubArray Sum/Solution.java
@@ -0,0 +1,25 @@
+public class Solution{
+  public static void main(String[] args) {
+    int[] array ={1,4,2,6,8};
+
+
+
+
+
+
+
+
+
+  }
+
+
+  public static void SubArray(int[] arr,int start,int end,int k){
+   Stack<Intger> stack=new Stack<Integer>
+
+   stack.push(arr[start]);
+
+
+
+
+  }
+}
