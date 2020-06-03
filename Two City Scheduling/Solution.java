9d46860bfee1e5c8a84b33a5cc39a14a93a3ed4a
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-03 18:52:53 +0530

    UPDATE

--- Two City Scheduling/Solution.java
+++ Two City Scheduling/Solution.java
@@ -4,7 +4,7 @@ public class Solution{
 
 
 
-// lambda function
+// lambda function SUPPOURTED ONLY ON JAVA 8
     Arrays.sort(costs, (o1, o2) -> (o1[0] - o1[1]) - (o2[0] - o2[1]));
            int res = 0, i = 0, j = costs.length - 1;
            while(i < j) {
