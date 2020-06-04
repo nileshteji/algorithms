c3462e0d2cf5e4663fd72b2a43bb31470a030883
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-04 23:58:30 +0530

    commit 21

--- Kadane Algorithm/Solution.java
+++ Kadane Algorithm/Solution.java
@@ -1,31 +1,27 @@
-// Java program to print largest contiguous
-// array sum
-import java.io.*;
+class Solution{
+  public static void main(String[] args) {
 
-class Solution {
+int[] array={-2, -3, 2, -1, -2, 1, 5, -3};
+System.out.println(maxiumSum(array));
 
-	static int maxSubArraySum(int a[], int size)
-	{
-	int max_so_far = a[0];
-	int curr_max = a[0];
 
-	for (int i = 1; i < size; i++)
-	{
-		curr_max = Math.max(a[i], curr_max+a[i]);
-		max_so_far = Math.max(max_so_far, curr_max);
-	}
-	return max_so_far;
-	}
+  }
 
-	/* Driver program to test maxSubArraySum */
-	public static void main(String[] args)
-	{
-	int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
-	int n = a.length;
-	int max_sum = maxSubArraySum(a, n);
-	System.out.println("Maximum contiguous sum is "
-					+ max_sum);
-	}
+
+  static int maxiumSum(int[] array){
+
+int current_Sum=0;
+int overall_Sum=0;
+
+    for(int i : array){
+      current_Sum=Math.max(i,current_Sum+i);
+      overall_Sum=Math.max(current_Sum,overall_Sum);
+    }
+
+return overall_Sum;
+
+
+
+
+  }
 }
-
-// This code is contributd by Prerna Saini
