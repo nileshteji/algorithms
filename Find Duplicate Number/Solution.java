9a8a2bbd3cf60fb5a8105ccebbc6212e596c926e
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-10 15:47:23 +0530

    Find Duplicate Number

--- Find Duplicate Number/Solution.java
+++ Find Duplicate Number/Solution.java
@@ -1,7 +1,7 @@
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = { 1, 2, 3, 3, 4, 5 };
+        int[] nums = { 1, 2,3,4,4, 5 };
         int a = findNums(nums);
         System.out.println(a);
     }
@@ -10,8 +10,22 @@ public class Solution {
 
         // TODO: 1 2 3 3 4 5
 
+        // slow 1 fast 2 
+        // slow 2 fast 3
+        // slow 3 fast 3 
+        // slow 3 fast 0
+        // slow 3 fast 1 
+        // slow 3 fast 2
+        // slow 3 fast 3
+
+
+
+
+
         int slow = nums[0];
         int fast = nums[nums[0]];
+        
+
 
         while (slow != fast) {
             slow = nums[slow];
@@ -26,7 +40,7 @@ public class Solution {
             fast = nums[fast];
         }
 
-        return slow;
+        return fast;
 
     }
 }
