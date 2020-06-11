254bc72dd17eeb2070755f900a0160126d554641
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-11 21:52:09 +0530

    Day 2

--- Rotation Matrix/Solution.java
+++ Rotation Matrix/Solution.java
@@ -1,5 +1,27 @@
 class Solution{
   public static void main(String[] args) {
-    
+    public class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = i; j<matrix[0].length; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+
+
+        for(int i =0 ; i<matrix.length; i++){
+            for(int j = 0; j<matrix.length/2; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }
+}
   }
 }
