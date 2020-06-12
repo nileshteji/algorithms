5765eda350f1e6bebe95404b0206e9984c5b30ab
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-12 16:55:39 +0530

    Count Trailing Zeros

---
+++ Count Trailing Zero/Solution.java
@@ -0,0 +1,29 @@
+class Solution{
+  public static void main(String[] args) {
+    int number =5;
+
+  }
+
+// one approach is usign brute force calculate the factorial and then use a pointer from the end
+// to count the number of zeroes.............
+
+
+
+// a zero is always prodduced using prime factors of 2 and 5 .
+
+
+// Simple approach is using the formula
+  static int solve(int a){
+
+
+int zeros=0;
+
+
+// we are looping till we are not getting i greater than or equal to the number
+for(int i=5;n/i>=1;i=i*5){
+  zeros=zeros+n/i;
+}
+
+return zeros;
+  }
+}
