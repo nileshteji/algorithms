db45f6963cb6630c37cffbdef276022f636d63de
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-14 03:20:49 +0530

    commit

--- Longest Consecutive SubString/Solution.java
+++ Longest Consecutive SubString/Solution.java
@@ -1,3 +1,20 @@
+import java.util.HashMap;
+import java.util.Set;
+
 public class Solution {
+static int ans=0;
+    public static void main(String[] args) {
+        String a = "abbabbb";
+        System.out.print(solve(a));
+        
+    }
+
+    static int solve(String a) {
+
     
+
+
+
+    }
+
 }
