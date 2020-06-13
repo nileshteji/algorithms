cd168d9dbfcd1be41fa9621bf872961fd9e0a5d0
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-13 23:25:38 +0530

    Explaination of Subarray with sum Zero

--- SubArray with Zero/Solution.java
+++ SubArray with Zero/Solution.java
@@ -1,42 +1,64 @@
-import java.util.HashMap;
 
-public class Solution {
-    public static void main(String[] args) {
-        int arr[] = { 15, -2, 2, -8, 1, 7, 10, 23 };
-    }
+import java.util.HashMap; 
 
-    static void solve(int[] array) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
+class MaxLenZeroSumSub { 
 
-        int current_Sum = 0;
-        int maxLength = 0;
+	
+	static int maxLen(int arr[]) 
+	{ 
+	
+		HashMap<Integer, Integer> hM = new HashMap<Integer, Integer>(); 
 
-        for (int i = 0; i < array.length; i++) {
+		int sum = 0; 
+		int max_len = 0; 
 
-            if (array[i] == 0 && current_Sum == 0) {
-                maxLength++;
+		for (int i = 0; i < arr.length; i++) { 
+		
+			sum += arr[i]; 
 
-            } else if (array[i] == 0) {
-                maxLength = i + i;
-            }
+			if (arr[i] == 0 && max_len == 0) 
+				max_len = 1; 
 
-            Integer a = hashMap.get(current_Sum);
+			if (sum == 0) 
+				max_len = i + 1; 
 
-            if (a == null) {
-                maxLength = Math.max(maxLength, i - a);
-            } else {
-                hashMap.put(current_Sum, i);
-            }
+		 
+			Integer prev_i = hM.get(sum); 
 
-        }
+			
+			if (prev_i != null) 
+				max_len = Math.max(max_len, i - prev_i); 
+			else 
+				hM.put(sum, i); 
+		} 
 
-    }
-}
+		return max_len; 
+	} 
 
-//TODO:Full Explaination
+	
+	public static void main(String arg[]) 
+	{ 
+		int arr[] = { 15, -2, 2, -8, 1, 7, 10, 23 }; 
+		System.out.println("Length of the longest 0 sum subarray is "
+						+ maxLen(arr)); 
+	}
+} 
 
-// So what we will do is we will be using 
 
 
 
 
+// Explaination
+
+
+// In this we will take a varoable by the the name of Current Sum and MaxLength
+// which will be our final answer;
+// So if we see current_Sum which is zero then we update the length of the maxLength
+// to i +1;
+
+// if see unique Current_Sum value we will be put the current sum into hashmap 
+// with the index of the current_sum and 
+// if see that the current sum repeats itself then it means that the numbers
+// in between the previous current_sum and the present  current_sum is zero 
+// so we find the mx val of the maxlength and the index of present current sum 
+// previous sum 
