5333bc7d3f4e1382d067aeee9dd8688b0cf09722
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-24 19:09:28 +0530

    BFS

--- BFS/Solution.java
+++ BFS/Solution.java
@@ -1,16 +1,13 @@
-// Java program to print BFS traversal from a given source vertex. 
-// BFS(int s) traverses vertices reachable from s. 
+ 
 import java.io.*; 
 import java.util.*; 
 
-// This class represents a directed graph using adjacency list 
-// representation 
 class Graph 
 { 
-	private int V; // No. of vertices 
-	private LinkedList<Integer> adj[]; //Adjacency Lists 
+	private int V; 
+	private LinkedList<Integer> adj[]; 
 
-	// Constructor 
+	
 	Graph(int v) 
 	{ 
 		V = v; 
@@ -19,36 +16,31 @@ class Graph
 			adj[i] = new LinkedList(); 
 	} 
 
-	// Function to add an edge into the graph 
 	void addEdge(int v,int w) 
 	{ 
 		adj[v].add(w); 
 	} 
 
-    // prints BFS traversal from a given source s 
-    
+  
 	void BFS(int s) 
 	{ 
-		// Mark all the vertices as not visited(By default 
-		// set as false) 
+		 
 		boolean visited[] = new boolean[V]; 
 
-		// Create a queue for BFS 
+		
 		LinkedList<Integer> queue = new LinkedList<Integer>(); 
 
-		// Mark the current node as visited and enqueue it 
+
 		visited[s]=true; 
 		queue.add(s); 
 
 		while (queue.size() != 0) 
 		{ 
-			// Dequeue a vertex from queue and print it 
+		
 			s = queue.poll(); 
 			System.out.print(s+" "); 
 
-			// Get all adjacent vertices of the dequeued vertex s 
-			// If a adjacent has not been visited, then mark it 
-			// visited and enqueue it 
+		
 			Iterator<Integer> i = adj[s].listIterator(); 
 			while (i.hasNext()) 
 			{ 
@@ -86,4 +78,4 @@ class Graph
 		g.BFS(2); 
 	} 
 } 
-// This code is contributed by Aakash Hasija 
+
