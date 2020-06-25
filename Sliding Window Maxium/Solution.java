8eac4f06c0f3088589f9cbe934ae82aa0a60d35b
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-25 21:30:11 +0530

    Sliding Widow Maxium

--- Sliding Window Maxium/Solution.java
+++ Sliding Window Maxium/Solution.java
@@ -1,5 +1,40 @@
+import java.util.*;
 
 
-public class Solution {
-    
+public class Solution{
+    public static void main(String[] args) {
+        int arr[] = { 12, 1, 78, 90, 57, 89, 56 }; 
+        int k = 3; 
+        printMax(arr, arr.length, k); 
+    }
+
+    private static void printMax(int[] arr, int length, int k) {
+
+        Deque<Integer> deque =new LinkedList<>();
+
+
+        for(int i =0;i<k;i++){
+          
+             while(!deque.isEmpty() && arr[i]>deque.peekLast())
+                 deque.pollLast();
+             deque.add(arr[i]);
+        }
+
+
+
+        for(int i =k ;i<length;i++){
+
+         
+
+            
+
+
+
+
+        }
+
+
+
+
+    }
 }
