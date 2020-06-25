02edd1d9387e6d866640f47789f232aa8061bcfd
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-25 19:15:15 +0530

    LRU Cache

--- LRU Cache/Solution.java
+++ LRU Cache/Solution.java
@@ -1,54 +1,78 @@
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
+/* We can use Java inbuilt Deque as a double 
+ended queue to store the cache keys, with 
+the descending time of reference from front 
+to back and a set container to check presence 
+of a key. But remove a key from the Deque using 
+remove(), it takes O(N) time. This can be 
+optimized by storing a reference (iterator) to 
+each key in a hash map. */
+import java.util.Deque; 
+import java.util.HashSet; 
+import java.util.LinkedList; 
+import java.util.Iterator; 
+public class Solution { 
 
-class Cache {
-    int cache_size = 3;
+	 
+	static Deque<Integer> dq; 
 
-    HashSet<Integer> set = new HashSet<>();
-    Deque<Integer> deque = new LinkedList<>();
+	
+	static HashSet<Integer> map; 
 
-    Cache(int n) {
-        cache_size = n;
-    }
+	
+	static int csize; 
 
-    public void refer(int x) {
+	Solution(int n) 
+	{ 
+		dq = new LinkedList<>(); 
+		map = new HashSet<>(); 
+		csize = n; 
+	} 
 
-        if (!deque.contains(x)) {
-            if (deque.size() == cache_size) {
-                int a = deque.removeLast();
-                set.remove(a);
-            } else {
-                deque.add(x);
-                set.add(x);
 
-            }
-        } else {
-            Iterator<Integer> nIterator = deque.iterator();
-            while (nIterator.hasNext()) {
-                if (nIterator.next() == x) {
-                    deque.removeFirstOccurrence(x);
-                    deque.push(x);
+	public void refer(int x) 
+	{ 
+		if (!map.contains(x)) { 
+			if (dq.size() == csize) { 
+				int last = dq.removeLast(); 
+				map.remove(last); 
+			} 
+		} 
+		else { 
+			
+			int index = 0, i = 0; 
+			Iterator<Integer> itr = dq.iterator(); 
+			while (itr.hasNext()) { 
+				if (itr.next() == x) { 
+					index = i; 
+					break; 
+				} 
+				i++; 
+			} 
+			dq.remove(index); 
+		} 
+		dq.push(x); 
+		
+	} 
 
-                }
-            }
+	
+	public void display() 
+	{ 
+		Iterator<Integer> itr = dq.iterator(); 
+		while (itr.hasNext()) { 
+			System.out.print(itr.next() + " "); 
+		} 
+	} 
 
-        }
-    }
+	public static void main(String[] args) 
+	{ 
+		Solution ca = new Solution(4); 
+		ca.refer(1); 
+		ca.refer(2); 
+		ca.refer(3); 
+		ca.refer(1); 
+		ca.refer(4); 
+		ca.refer(5); 
+		ca.display(); 
+	} 
+} 
 
-    public static void main(String[] args) {
-        Cache ca = new Cache(4);
-        ca.refer(1);
-        ca.refer(2);
-        ca.refer(3);
-        ca.refer(1);
-        ca.refer(4);
-        ca.refer(5);
-        ca.display();
-    }
-
-    private void display() {
-        System.out.println(deque);
-    }
-}
