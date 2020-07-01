e16ecbdd6a0444c8b96ec542e31ac2d2ce938bf2
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-07-01 16:14:38 +0530

    Inorder Traversal

--- Inorder Traversal/Solution.java
+++ Inorder Traversal/Solution.java
@@ -1,7 +1,44 @@
 import java.util.*;
-public class Solution{
-    public static void main(String[] args) {
 
-Scanner scanner=new  Scanner(System.in);
+class Node 
+{ 
+    int data; 
+    Node left, right; 
+  
+    public Node(int item) 
+    { 
+        data = item; 
+        left = right = null; 
+    } 
+} 
+  
+public class Solution{
+
+static void printInorder(Node root){
+
+
+    if(root==null){
+        return;
+    }
+     
+    printInorder(root.left);
+    System.out.println(root.data);
+    printInorder(root.right);
+  
+
+
+
+
+
+
+
+}
+    public static void main(String[] args) {
+Node root=new Node(1);
+root.left=new Node(2);
+root.left.left=new Node(3);
+root.left.right=new Node(4);
+root.right=new Node(5);
+printInorder(root);
     }
 }
