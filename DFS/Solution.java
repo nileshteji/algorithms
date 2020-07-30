edb57286cc9b285002f5520d2b2a1dd1041602cc
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-07-30 07:52:15 +0530

    DFS

---
+++ DFS/Solution.java
@@ -0,0 +1,71 @@
+import java.io.*; 
+import java.util.*; 
+
+
+class Graph 
+{ 
+	private int V; 
+     // neighbour linked list for every node;
+	private LinkedList<Integer> adj[]; 
+   Graph(int v) 
+	{ 
+		V = v; 
+		adj = new LinkedList[v]; 
+		for (int i=0; i<v; ++i) 
+			adj[i] = new LinkedList(); 
+	} 
+
+	
+	void addEdge(int v, int w) 
+	{ 
+		adj[v].add(w); 
+	} 
+
+	
+	void DFSUtil(int v,boolean visited[]) 
+	{ 
+		
+		visited[v] = true; 
+		System.out.print(v+" "); 
+
+		
+		Iterator<Integer> i = adj[v].listIterator(); 
+		while (i.hasNext()) 
+		{ 
+			int n = i.next(); 
+            if (!visited[n]) 
+            //This call goes onto the call stack we use stack for dfs and queue for bfs
+            //DFS is better for finding shortest distance.
+				DFSUtil(n, visited); 
+		} 
+	} 
+
+	
+	void DFS(int v) 
+	{ 
+		
+		boolean visited[] = new boolean[V]; 
+
+		 
+		DFSUtil(v, visited); 
+	} 
+
+	public static void main(String args[]) 
+	{ 
+		Graph g = new Graph(4); 
+
+		g.addEdge(0, 1); 
+		g.addEdge(0, 2); 
+		g.addEdge(1, 2); 
+		g.addEdge(2, 0); 
+		g.addEdge(2, 3); 
+		g.addEdge(3, 3); 
+
+		System.out.println("Following is Depth First Traversal "+ 
+						"(starting from vertex 2)"); 
+
+		g.DFS(2); 
+	} 
+} 
+
+
