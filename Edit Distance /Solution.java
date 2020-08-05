927249a2862bc6d6201715d230931f4591cbec85
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-08-05 17:52:19 +0530

    Edit Distance

---
+++ Edit Distance /Solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public static void main(String[] args) {
+        String a="abcde";
+        String b="abc";
+    int an=find(a,b);
+System.out.println(an);
+    }
+    static int find(String a,String b){
+        int[][] dp=new int[b.length()+1][a.length()+1];
+        
+        for(int i =0;i<=b.length();i++){
+            for(int j =0;j<=a.length();j++){
+                if(i==0 ){
+                    dp[i][j]=j;
+                }
+                else if(j==0){
+                    dp[i][j]=i;
+                }
+                else{
+                    if(a.charAt(i-1)==b.charAt(j-1)){
+                        dp[i][j]=dp[i-1][j-1];
+                    }
+                    else{
+                        dp[i][j]=Math.min(dp[i-1][j], dp[i][j-1])+1;
+                    }
+                }
+                
+            }
+        }
+        return dp[b.length()][a.length()];
+    }
+}
