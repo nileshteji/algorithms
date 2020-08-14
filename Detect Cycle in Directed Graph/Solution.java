eede8c68b102f566e5ec434e4b684e7f5b2347f7
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-08-14 20:19:02 +0530

    d

--- Detect Cycle in Directed Graph/Solution.java
+++ Detect Cycle in Directed Graph/Solution.java
@@ -1,25 +1,27 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
-class Graph{
+class Graph {
     int vertices;
     ArrayList<ArrayList<Integer>> adj;
-    public Graph(int vertices){
-        this.vertices=vertices;
-        for(int i =0;i<vertices;i++){
-            adj=new ArrayList<>();
+
+    public Graph(int vertices) {
+        this.vertices = vertices;
+        for (int i = 0; i < vertices; i++) {
+            adj = new ArrayList<>();
         }
     }
 
-    public  void addEdge(int src,int destination){
+    public void addEdge(int src, int destination) {
         adj.get(src).add(destination);
     }
 
 }
 
-
-// we can use simple dfs if we get the visited node again then it means it is a cyclic graph
-class Solution{
+// we can use simple dfs if we get the visited node again then it means it is a
+// cyclic graph
+class Solution {
     public static void main(String[] args) {
-        
-    } 
+
+    }
 }
