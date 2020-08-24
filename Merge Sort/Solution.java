626a845e4a0171784b10beb25c11620d98c8bcd5
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-08-25 00:44:48 +0530

    Done

--- Merge Sort/Solution.java
+++ Merge Sort/Solution.java
@@ -1,18 +1,92 @@
-class Solution{
-    public static void main(String[] args) {
-        int arr[] = { 12, 11, 13, 5, 6, 7 }; 
+/* Java program for Merge Sort */
+class MergeSort { 
+	
+	void merge(int arr[], int l, int m, int r) 
+	{ 
+		
+		int n1 = m - l + 1; 
+		int n2 = r - m; 
 
-    }
-    static void sort(int[] array,int start,int end){
-        if(l<r){
-        int m =(l+r)/2;
-        // in this we are recurisely solving for the merge untukr this 
-        // we are continously breaking the merge sort problem fo
-        sort(array, start,m);
-        sort(array, m+1, end);
-        }
-    }
+		
+		int L[] = new int[n1]; 
+		int R[] = new int[n2]; 
+
+		
+		for (int i = 0; i < n1; ++i) 
+			L[i] = arr[l + i]; 
+		for (int j = 0; j < n2; ++j) 
+			R[j] = arr[m + 1 + j]; 
+
+		
+		int i = 0, j = 0; 
+
+		
+		int k = l; 
+		while (i < n1 && j < n2) { 
+			if (L[i] <= R[j]) { 
+				arr[k] = L[i]; 
+				i++; 
+			} 
+			else { 
+				arr[k] = R[j]; 
+				j++; 
+			} 
+			k++; 
+		} 
+
+		
+		while (i < n1) { 
+			arr[k] = L[i]; 
+			i++; 
+			k++; 
+		} 
+
+	
+		while (j < n2) { 
+			arr[k] = R[j]; 
+			j++; 
+			k++; 
+		} 
+	} 
 
 
-    
-}
+	void sort(int arr[], int l, int r) 
+	{ 
+		if (l < r) { 
+			
+			int m = (l + r) / 2; 
+
+			
+			sort(arr, l, m); 
+			sort(arr, m + 1, r); 
+
+			
+			merge(arr, l, m, r); 
+		} 
+	} 
+
+	
+	static void printArray(int arr[]) 
+	{ 
+		int n = arr.length; 
+		for (int i = 0; i < n; ++i) 
+			System.out.print(arr[i] + " "); 
+		System.out.println(); 
+	} 
+
+	// Driver method 
+	public static void main(String args[]) 
+	{ 
+		int arr[] = { 12, 11, 13, 5, 6, 7 }; 
+
+		System.out.println("Given Array"); 
+		printArray(arr); 
+
+		MergeSort ob = new MergeSort(); 
+		ob.sort(arr, 0, arr.length - 1); 
+
+		System.out.println("\nSorted array"); 
+		printArray(arr); 
+	} 
+} 
+/* This code is contributed by Rajat Mishra */
