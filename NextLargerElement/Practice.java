cab4dc1fef06a0b9764eee1ca21fbad093c993da
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-08-29 17:45:56 +0530

    Modification

---
+++ NextLargerElement/Practice.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+
+
+public class Practice {
+    public static void main(String[] args) {
+        int[] main = { 4, 3, 6, 1, 10 };
+        find(main);
+    }
+
+    static void find(int[] array) {
+        Stack<Integer> stack = new Stack<>();
+        // if we want to return the array we can use index insted of the value
+        // Similar like the Sliding window maxium
+        stack.push(array[0]);
+
+        for (int i : array) {
+            while (stack.isEmpty() == false && stack.peek() < i) {
+                System.out.println(stack.pop() + " " + i);
+            }
+
+            stack.add(i);
+
+        }
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop() + " " + 0);
+        }
+    }
+}
