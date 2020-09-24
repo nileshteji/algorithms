688858539221321fa23b7f452c7ccaad32e0aea3
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-09-24 19:09:18 +0530

    Binary Search(Alphabetical Order)

--- Next Alphabetical Character/Solution.java
+++ Next Alphabetical Character/Solution.java
@@ -1,18 +1,38 @@
 public class Solution {
     public static void main(String[] args) {
         char[] array={
-            a,c,f,h
-        };
-        solve(array,c);≥≥
+            'a','c','f','h'};
+        solve(array,'d');
     }
 
-    public static void solve(char[] array,char){
+    public static void solve(char[] array,char c){
         int low=0;
         int high=array.length-1;
+        int res=-1;
 
         while(low<=high){
+            
+            int mid=low+high/2;
+
+            if(array[mid]==c){
+                System.out.println(mid+1);
+                return;
+            }
+
+            else if((int) array[mid] > (int)c){
+                   res=mid;
+                   high=mid-1;
+                   System.out.println(res);    
+            }
+            else if((int) array[mid] < (int) c){
+                low=mid+1;
+                System.out.println(res);
+            }
 
         }
+
+        System.out.println(res);
+
     }
 
 
