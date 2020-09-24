bf39260dcd04f42a0a4f4d1a09206275f802ec3b
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-09-24 14:30:10 +0530

    Binary Search(Alphabtical Order)

---
+++ Next Alphabetical Character/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public static void main(String[] args) {
+        char[] array={
+            a,c,f,h
+        };
+        solve(array,c);≥≥
+    }
+
+    public static void solve(char[] array,char){
+        int low=0;
+        int high=array.length-1;
+
+        while(low<=high){
+
+        }
+    }
+
+
+}
