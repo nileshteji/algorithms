873a67e7bcb50da42338ab2cade5007ee000d76e
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-09-24 19:29:22 +0530

    Search Element in Infinite Array

---
+++ Find Element in Infinite Array/Solution.java
@@ -0,0 +1,5 @@
+// In this as we are not given any high index in the Array 
+// we will take the high index  as 1 
+// we will increase the high index *2 and start index will be the high index 
+// now if the key is less than high
+// then we will do binary search in that array
