bfc2dc02b9582a4486389a9bb7f4b62758af2cf9
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-09-27 16:04:44 +0530

    Done

--- Practice One/Solution.java
+++ Practice One/Solution.java
@@ -1,70 +1,21 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+class Solution{
+    public static void main(String[] args) {
+        String a="Abhay Nand";
+        String b="Abhay";
+        System.out.println(a.concat("Maurya"));
+        System.out.println(a.equals(b));
+        System.out.println(a.equalsIgnoreCase(b));
+        System.out.println(a.toUpperCase());
+        System.out.println(a.charAt(0));
+        System.out.println(a.compareTo(b));
+        String c=" Abhay    ";
+        System.out.println(c.hashCode());
+        System.out.println(c.trim());
+        System.out.println(c.intern());
+        System.out.println(c.length());
+        System.out.println(c.replace('A', 'N'));
+        System.out.println(c.substring(0,2));
 
-class Result {
-
-public static long getWays(int n, List<Long> c) {
-
-    
-    // Write your code here
-    long[][] dp=new long[(int)c.size()+1][(int)n+1];
-
-    for(int i =0;i<=c.size();i++){
-        dp[i][0]=1;
-    }
-    for(int j=0;j<=c.size();j++){
-        dp[0][j]=0;
-    }
-
-    for(int i =1;i<=c.size();i++){
-        for(int j =1;j<=n;i++){
-            if(c.get(i-1)<=j){
-             
-                dp[i][j]=dp[i][j-c.get(i-1)];
-            }
-            else{
-                dp[i][j]=dp[i-1][j];
-            }
-        }
-    }
-    return dp[c.size()][n];
-}
-
-}
-
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        int n = Integer.parseInt(firstMultipleInput[0]);
-
-        int m = Integer.parseInt(firstMultipleInput[1]);
-
-        String[] cTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        List<Long> c = new ArrayList<>();
-
-        for (int i = 0; i < m; i++) {
-            long cItem = Long.parseLong(cTemp[i]);
-            c.add(cItem);
-        }
-
-        // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
-
-        long ways = Result.getWays(n, c);
-
-        bufferedWriter.write(String.valueOf(ways));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        
     }
 }
