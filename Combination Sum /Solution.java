9d4cd2fffbc883cd324d09f05c0b3b47f6da0f42
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-10-10 14:53:20 +0530

    Solution

--- Combination Sum /Solution.java
+++ Combination Sum /Solution.java
@@ -1,18 +1,34 @@
-import java.util.*;
-public class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
-        backtrack(list, new ArrayList<>(), nums, 0);
-        return list;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+
+class Solution{
+    public static void main(String[] args) {
+        int[] array={2,3,6,7};
+        int target=7;
+        List<List<Integer>> allPart=new ArrayList<>();
+        Deque<Integer> currPart=new LinkedList<>();
+        backTrack(target, allPart, currPart,array,0);
+        System.out.println(allPart);
+
     }
-    
-    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
-        list.add(new ArrayList<>(tempList));
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, i + 1);
-            tempList.remove(tempList.size() - 1);
+
+    private static  void backTrack(int target,List<List<Integer>> allPart,Deque<Integer> currPart,int[] array,int start){
+        if(target==0){
+            allPart.add(new ArrayList<>(currPart));
+            return;
         }
+        if(target<0){
+            return;
+        }
+
+        for(int i =start;i<array.length;i++){
+            currPart.add(array[i]);
+            backTrack(target-array[i], allPart, currPart, array,i);
+            currPart.removeLast();
+        }
+
     }
 }
--- Practice One/Solution.java
+++ Practice One/Solution.java
@@ -1,12 +1,37 @@
-class ThreadOne implements Runnable{
-	@Override
-	public void run(){
-		System.out.println("Abhay Maurya");
-	}
-}
-class Solution{
+import java.util.HashMap;
+import java.util.Map;
+
+class Sol{
 	public static void main(String[] args) {
-		Thread abhay=new Thread(new ThreadOne());
-		abhay.start();
+		int[] array ={4,8,8,8,6,4};
+		// 3 6    4  8 /8 6 4 
+		int k=3;
+		Map<Integer,Integer> map=new HashMap<>();
+		for (int i :array){
+			if(map.get(i)!=null){
+				map.put(i,map.get(i)+1);
+			}
+			else{
+				map.put(i, 1);
+			}
+		}
+
+		boolean ans=true;
+
+		if(array.length%k!=0){
+			ans=false;
+		}
+		else{
+
+	  for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+		  
+		  if(entry.getValue()!=1 && entry.getValue()>array.length/k){
+			  ans=false;
+		  }
+	  }
+	}
+
+		System.out.println((ans)?"Yes":"No");
+
 	}
 }
