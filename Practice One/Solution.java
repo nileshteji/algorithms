4d53f01c40082172b9df9383958a88f0ff70f2e6
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-10-30 23:44:29 +0530

    Practice

--- Practice One/Solution.java
+++ Practice One/Solution.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
-import jdk.jfr.Percentage;
-
 class Solution{
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        String a=scan.next();
-        if(scan.next().equalsIgnoreCase("Y")){
-           String child=scan.next();
-           String[] childArray=child.split(",");
-           int temp=childArray.length+1;
-           System.out.println("TOTAL MEMBERS:"+temp);
-           System.out.println("COMMISSION DETAILS");
-           int answer= childArray.length * (500);
-           System.out.println(a+":"+answer);
-           for(String s : childArray){
-               System.out.println(s+":"+(500/2));
-           }
+          int a=scan.nextInt();
+          for(int i =2;i<a;i++){
+              if(isPrime(i)){
+                System.out.println(i);
+              }
 
-        }
-        else{
+          }
 
+
+    }
+
+    public static boolean isPrime(int a){
+
+        for(int i =2;i<a;i++){
+            if(a%i==0){
+                return false;
+            }
         }
+        return true;
     }
 }
